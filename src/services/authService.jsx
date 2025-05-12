ac4163426696d945a80a9c93b3f239c376014e8a
phonglamm <lamphong1507@gmail.com>
2025-05-12 22:09:21 +0700

    setup services

---
+++ src/services/authService.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const request = async (method, url, data = null, headers = {}, params = {}) => {
+  try {
+    const response = await axios({
+      method,
+      url: `${API_BASE_URL}${url}`,
+      data,
+      headers: {
+        ...headers,
+      },
+      params,
+    });
+
+    return response.data;
+  } catch (error) {
+    console.error("API Error:", error);
+    throw error;
+  }
+};
+
+const authService = {
+  login: (loginData) => request("POST", "v1/auth", loginData),
+};
+
+export default authService;
